7a3c7a885dcc20abc41aaeb9a91b79c36561e663
kraulain <kraulain@gmail.com>
2023-06-16 11:25:12 +0100

    use chesire to parse json from strings

--- src/clj_b_7/core.clj
+++ src/clj_b_7/core.clj
@@ -3,6 +3,12 @@
 
 (defonce books-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/books.json"))
 
-(defonce character-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/characters.json"))
+(defonce characters-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/characters.json"))
 
 (defonce houses-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/houses.json"))
+
+(defonce books-json (json/parse-string books-raw))
+
+(defonce characters-json (json/parse-string characters-raw))
+
+(defonce houses-json (json/parse-string houses-raw))
