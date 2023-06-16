a465a78947a1e17850edb3426458fda06ba8200e
kraulain <kraulain@gmail.com>
2023-06-16 10:32:37 +0100

    Slurp json files from github raw

--- src/clj_b_7/core.clj
+++ src/clj_b_7/core.clj
@@ -1,6 +1,8 @@
-(ns clj-b-7.core)
+(ns clj-b-7.core
+  (:require [cheshire.core :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce books-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/books.json"))
+
+(defonce character-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/characters.json"))
+
+(defonce houses-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/data/houses.json"))
